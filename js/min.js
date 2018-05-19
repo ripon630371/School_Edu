71cf4e7b08ae5eb2f48129a434f41433a7e406d5
ripon630371 <riponcmt630371@gmail.com>
2018-05-19 16:02:20 +0600

    welcome section ok

---
+++ js/min.js
@@ -0,0 +1,38 @@
+//future section area owlcarousel .....................
+$('.future-owl').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        }
+	    }
+})
+//future section area owlcarousel .....................
+$('.course-owl').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    navText:['<i class="fas fa-chevron-left"></i>','<i class="fas fa-chevron-right"></i>'],
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:3
+	        },
+	        1000:{
+	            items:3
+	        }
+	    }
+})
