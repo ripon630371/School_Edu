3cdc209222e002ffc1459f85e9a3e87147e68005
ripon630371 <riponcmt630371@gmail.com>
2018-05-23 00:39:38 +0600

    why choose area ok

--- js/min.js
+++ js/min.js
@@ -35,4 +35,24 @@ $('.course-owl').owlCarousel({
 	            items:3
 	        }
 	    }
-})
+});
+// why-carousel section area owlcarousel .....................
+$('.why-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    autoplay:true,
+	    autoplayTimeout:4000,
+	    dots:false,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        600:{
+	            items:1
+	        },
+	        1000:{
+	            items:1
+	        }
+	    }
+});
+//why chosse section......................................
